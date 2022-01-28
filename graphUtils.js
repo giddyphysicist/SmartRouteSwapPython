1bb1e6b7183a78a1aa4edeea08b052453daa12f9
Patrick Gartland <giddyphysicist@gmail.com>
2022-01-27 19:23:34 -0600

    Update graphUtils.js
    added support functions, array functions, and wrapper function for k shortest paths

--- graphUtils.js
+++ graphUtils.js
@@ -1,36 +1,35 @@
-const { assert } = require("console");
+const { assert } = require('console');
 
-function addEdge(g,edge) {
+function addEdge(g, edge) {
     let src = edge[0];
     let dst = edge[1];
     if (Object.keys(g).includes(src)) {
         if (!Object.keys(g[src]).includes(dst)) {
             g[src][dst] = 1;
-        };
+        }
     } else {
-        g[src]={};
-        g[src][dst]=1;
-    };
+        g[src] = {};
+        g[src][dst] = 1;
+    }
     if (Object.keys(g).includes(dst)) {
         if (!Object.keys(g[dst]).includes(src)) {
             g[dst][src] = 1;
-        };
+        }
     } else {
         g[dst] = {};
-        g[dst][src]=1;
-    };
-};
-
+        g[dst][src] = 1;
+    }
+}
 
 function addEdges(g, edgeList) {
     for (var n in edgeList) {
         let edge = edgeList[n];
         //console.log(edge);
         addEdge(g, edge);
-    };
-};
+    }
+}
 
-function deleteEdge(g,edge) {
+function deleteEdge(g, edge) {
     let gNew = JSON.parse(JSON.stringify(g)); // using this to deep clone graph structure
     let e1 = edge[0];
     let e2 = edge[1];
@@ -47,12 +46,12 @@ function deleteEdge(g,edge) {
     return gNew;
 }
 
-function deleteNode(g,node) {
+function deleteNode(g, node) {
     let gNew = JSON.parse(JSON.stringify(g)); // using this to deep clone graph structure
     if (Object.keys(gNew).includes(node)) {
         delete gNew[node];
     }
-    let keys = Object.keys(gNew)
+    let keys = Object.keys(gNew);
     for (var nodeInd in keys) {
         let nodeNow = keys[nodeInd];
         if (Object.keys(gNew[nodeNow]).includes(node)) {
@@ -62,87 +61,77 @@ function deleteNode(g,node) {
     return gNew;
 }
 
-  function dijkstra(graph, s) {
+function dijkstra(graph, s) {
     var solutions = {};
     solutions[s] = {};
     solutions[s].path = [];
     solutions[s].dist = 0;
-    
-    while(true) {
-      var parent = null;
-      var nearest = null;
-      var dist = Infinity;
-      
-      //for each existing solution
-      for(var n in solutions) {
-        if(!solutions[n]) {
-            solutions[n]={};
-        }
-        if(!solutions[n].path)
-          continue
-        var ndist = solutions[n].dist;
-        var adj = graph[n];
-        //for each of its adjacent nodes...
-        for(var a in adj) {
-          //without a solution already...
-          if(!solutions[a]) {
-              solutions[a] = {};
-          }
-          if(solutions[a].path)
-            continue;
-          //choose nearest node with lowest *total* cost
-          var d = adj[a] + ndist;
-          if(d < dist) {
-            //reference parent
-            parent = solutions[n].path;
-            nearest = a;
-            dist = d;
-          }
-        }
-      }
-      
-      //no more solutions
-      if(dist === Infinity) {
-          break;
-      }
-      
-      //extend parent's solution path
-      solutions[nearest].path = parent.concat(nearest);
-      //extend parent's cost
-      solutions[nearest].dist = dist;
-    }
-    
-    return solutions;
-  }
 
-  function shortestPath(g, src, dst,ignore_nodes=[],ignore_edges=[]) {
-    let gTemp = JSON.parse(JSON.stringify(g)); // using this to deep clone graph structure
+    while (true) {
+        var parent = null;
+        var nearest = null;
+        var dist = Infinity;
+
+        //for each existing solution
+        for (var n in solutions) {
+            if (!solutions[n]) {
+                solutions[n] = {};
+            }
+            if (!solutions[n].path) continue;
+            var ndist = solutions[n].dist;
+            var adj = graph[n];
+            //for each of its adjacent nodes...
+            for (var a in adj) {
+                //without a solution already...
+                if (!solutions[a]) {
+                    solutions[a] = {};
+                }
+                if (solutions[a].path) continue;
+                //choose nearest node with lowest *total* cost
+                var d = adj[a] + ndist;
+                if (d < dist) {
+                    //reference parent
+                    parent = solutions[n].path;
+                    nearest = a;
+                    dist = d;
+                }
+            }
+        }
+
+        //no more solutions
+        if (dist === Infinity) {
+            break;
+        }
+
+        //extend parent's solution path
+        solutions[nearest].path = parent.concat(nearest);
+        //extend parent's cost
+        solutions[nearest].dist = dist;
+    }
+
+    return solutions;
+}
+
+function shortestPath(g, src, dst, ignore_nodes = [], ignore_edges = []) {
+    let gTemp = JSON.parse(JSON.stringify(g)); // using this to deep clone graph structure. If we can use lodash, could use  _.cloneDeep(obj)
     // remove nodes
     for (var nodeInd in ignore_nodes) {
         let nodeNow = ignore_nodes[nodeInd];
-        gTemp = deleteNode(gTemp,nodeNow);
+        gTemp = deleteNode(gTemp, nodeNow);
     }
     // remove edges
     for (var edgeInd in ignore_edges) {
         let edgeNow = ignore_edges[edgeInd];
-        gTemp = deleteEdge(gTemp,edgeNow);
+        gTemp = deleteEdge(gTemp, edgeNow);
     }
-    let solution = dijkstra(gTemp,src)[dst];
-    //console.log('solution is...')
-    //console.log(solution)
-    // console.log('EFFECTIVE GRAPH IS...')
-    // console.log(gTemp)
-    // console.log('DIJKSTRA SOLUTION IS...')
-    // console.log(solution)
+    let solution = dijkstra(gTemp, src)[dst];
     solution.path.unshift(src); // original algorithm doesn't include source node in path
-    return solution
-  }
+    return solution;
+}
 
-
-
-function* count(firstval=0,step=1) {
+function* count(firstval = 0, step = 1) {
     let x = firstval;
-    while(true) {
+    while (true) {
         yield x;
         x = x + 1;
     }
@@ -158,10 +147,12 @@ class PathBuffer {
         return this.sortedpaths.length;
     }
 
-    push(cost,path) {
-        if (!this.paths.includes(path)) {
-            this.sortedpaths.push([cost,path]);
-            this.sortedpaths.sort(function (a,b) {return a[0]-b[0]})
+    push(cost, path) {
+        if (path && !arrayContains(this.paths, path)) {
+            this.sortedpaths.push([ cost, path ]);
+            this.sortedpaths.sort(function(a, b) {
+                return a[0] - b[0];
+            });
             //heappush(this.sortedpaths, (cost, this.counter.next().value,path));
             this.paths.push(path);
         }
@@ -170,104 +161,122 @@ class PathBuffer {
     pop() {
         //let val = heappop(this.sortedpaths);
         let val = this.sortedpaths.shift();
-        // console.log('val is ...')
-        // console.log(val)
         let cost = val[0];
         let path = val[1];
-        this.paths.splice((this.paths.indexOf(path)),1);
-        return path
+        this.paths.splice(this.paths.indexOf(path), 1);
+        return path;
     }
-
-
 }
 
 function arrayEquals(a, b) {
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index]);
+    return Array.isArray(a) && Array.isArray(b) && a.length === b.length && a.every((val, index) => val === b[index]);
 }
 
-
-
-function* yenFromPy(g,source,target) {
-    //adapted from the python implementation in networkx.algorithms.simple_paths.shortest_simple_paths()
-  let listA = [];
-  let listB = new PathBuffer();
-  let prev_path =  null;
-
-  while(true) {
-    if (!prev_path) {
-        sol = shortestPath(g,source,target);
-        let length = sol.dist;
-        let path = sol.path;
-        listB.push(length,path);
-    } else {
-      let ignore_nodes = [];
-      let ignore_edges = [];
-      for (var i=1;i<prev_path.length;i++) {
-        let root = prev_path.slice(0,i);
-        let root_length = root.length;
-        for (var pathInd in listA) {
-          let path = listA[pathInd];
-
-          if (arrayEquals(path.slice(0,i),root)) {
-              let edgeToIgnore = [path[i-1],path[i]];
-              // console.log('edge to ignore is...')
-              // console.log(edgeToIgnore)
-              ignore_edges.push(edgeToIgnore);
-              // console.log('all ignore_edges are...')
-              // console.log(ignore_edges)
-          }
+function arrayContains(arr, obj) {
+    // checks to see if the input array contains a reference object, obj, using
+    // JSON.stringify() .
+    let obj_json = JSON.stringify(obj);
+    for (itemInd in arr) {
+        if (JSON.stringify(arr[itemInd]) == obj_json) {
+            return true;
         }
-          try {
-                //console.log('trying to get shortest path using ignore_edges...')
-                //console.log(ignore_edges)
-                let sol = shortestPath(g,root[root.length-1],target,ignore_nodes=ignore_nodes,ignore_edges=ignore_edges);
-                let length = sol.dist;
-                let spur = sol.path;
-                path = root.slice(0,root.length-1).concat(spur);
-                listB.push(root_length + length, path);
-
-          } catch {
-                //console.log('.')
-          }
-          //console.log('root is now...')
-          //console.log(root)
-          //console.log('now ignoring node ...')
-          // console.log(root[root.length-1])
-          ignore_nodes.push(root[root.length-1]);
-
-        
-
-      }
     }
-    if (listB) {
-        // console.log(listB)
-        // for (var ii=0; ii<listB.sortedpaths.length;ii++) {
-        //     console.log(listB.sortedpaths[ii][1])
-        // }
-        let path = listB.pop();
-        //  console.log('path is...')
-        //  console.log(path)
-        yield path;
-        listA.push(path);
-        prev_path = path;
-    } else {
-        break;
-    }
-
+    return false;
 }
+
+function* yenFromPy(g, source, target) {
+    //adapted from the python implementation in networkx.algorithms.simple_paths.shortest_simple_paths()
+    let listA = [];
+    let listB = new PathBuffer();
+    let prev_path = null;
+
+    while (true) {
+        if (!prev_path) {
+            sol = shortestPath(g, source, target);
+            let length = sol.dist;
+            let path = sol.path;
+            listB.push(length, path);
+        } else {
+            let ignore_nodes = [];
+            let ignore_edges = [];
+            for (var i = 1; i < prev_path.length; i++) {
+                let root = prev_path.slice(0, i);
+                let root_length = root.length;
+                for (var pathInd in listA) {
+                    let path = listA[pathInd];
+
+                    if (arrayEquals(path.slice(0, i), root)) {
+                        let edgeToIgnore = [ path[i - 1], path[i] ];
+                        ignore_edges.push(edgeToIgnore);
+                    }
+                }
+                try {
+                    let sol = shortestPath(
+                        g,
+                        root[root.length - 1],
+                        target,
+                        (ignore_nodes = ignore_nodes),
+                        (ignore_edges = ignore_edges)
+                    );
+                    let length = sol.dist;
+                    let spur = sol.path;
+                    path = root.slice(0, root.length - 1).concat(spur);
+                    listB.push(root_length + length, path);
+                } catch (e) {
+                    //dont do anything.
+                }
+                ignore_nodes.push(root[root.length - 1]);
+            }
+        }
+        if (listB.sortedpaths) {
+            try {
+                let path = listB.pop();
+                yield path;
+                listA.push(path);
+                prev_path = path;
+            } catch (e) {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+function getKShortestPaths(g, source, target, k) {
+    let paths = [];
+    let gen = yenFromPy(g, source, target);
+    for (var n = 1; n <= k; n++) {
+        try {
+            let res = gen.next().value;
+            if (res && !arrayContains(paths, res)) {
+                paths.push(res);
+            }
+        } catch (e) {
+            break;
+        }
+    }
+    return paths;
 }
 
 //let g = {'t1':{'t2':1,'t3':1},'t2':{'t3':1},'t3':{'t1':1,'t4':1},'t4':{'t3':1}}
 
 let gg = {};
 //let edges = [['t1','t2'],['t1','t3'],['t3','t4']];
-let edges = [['c','d'],['c','e'],['d','f'],['e','d'],['e','f'],['e','g'],['f','g'],['f','h'],['g','h']];
+let edges = [
+    [ 'c', 'd' ],
+    [ 'c', 'e' ],
+    [ 'd', 'f' ],
+    [ 'e', 'd' ],
+    [ 'e', 'f' ],
+    [ 'e', 'g' ],
+    [ 'f', 'g' ],
+    [ 'f', 'h' ],
+    [ 'g', 'h' ]
+];
 
 //console.log(g);
-addEdges(gg,edges);
+addEdges(gg, edges);
 //console.log(g);
 //deleteNode(g,'c');
 //console.log(g);
@@ -279,16 +288,20 @@ addEdges(gg,edges);
 // console.log(g)
 // console.log(gg)
 
-
 // console.log(shortestPath(gg,'c','h'))
 // console.log(shortestPath(gg,'c','h',ignore_nodes=[],ignore_edges=[['c','d']]))
 // console.log(shortestPath(gg,'c','h'))
 // console.log(shortestPath(gg,'c','h',ignore_nodes=['f'],ignore_edges=[]))
 
-gen = yenFromPy(gg,'c','h')
-for (let value of gen) {
-  console.log(value);
-}
+// gen = yenFromPy(gg,'c','h')
+// for (let value of gen) {
+//   console.log(value);
+// }
+
+//console.log([...yenFromPy(gg,'c','h')])
+
+console.log(getKShortestPaths(gg, 'c', 'h', 18));
+
 // console.log(gen.next())
 // console.log('==================================')
 // console.log(gen.next())
@@ -331,29 +344,3 @@ for (let value of gen) {
 
 //console.log(typeof(solutions['t4'].dist));
 //console.log(Object.keys(solutions['t4']));
-
-  // function yen(g, source,sink,K) {
-  //   // NOT DONE!!!
-  //   assert(false)
-  //   //copy graph into temp graph structure
-  //   let tempG =JSON.parse(JSON.stringify(g)); // using this to deep clone graph structure
-  //   let A = [];
-  //   A.push(dijkstra(tempG,source)[sink]);
-  //   let B = [];
-  //   for (var k=1;k<=K;k++) {
-  //       for (var i=0;i<A[k-1].length-2;i++) {
-  //           let spurNode=A[k-1].path[i];
-  //           let rootPath=A[k-1].path.slice(0,i);
-
-  //           for(var pathInd in A) {
-  //               let p = A[pathInd];
-  //               if (rootPath === p.path.slice(0,i)) {
-
-  //               }
-  //           }
-
-  //       }
-  //   }
-
-
-  // };
